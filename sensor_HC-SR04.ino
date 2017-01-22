e433951b5bf5d41bbed732ce7ba4559595241b6d
Pau Torres <ptmonline@users.noreply.github.com>
2017-01-22 18:49:01 +0100

    Create sensor_HC-SR04.ino

---
+++ sensor_HC-SR04.ino
@@ -0,0 +1,34 @@
+int ping = 7;
+// unsigned long duration;
+// vel = distancia/tempo
+// delta time
+void setup()
+{
+	Serial.begin(9600);	
+}
+
+void loop()
+{
+	float duration;
+	float cm;
+  	float velocidad;
+	pinMode(ping, OUTPUT);
+	digitalWrite(ping, HIGH);
+	digitalWrite(ping, LOW);
+
+	pinMode(ping, INPUT);
+	duration = pulseIn(ping, HIGH);
+
+	cm = (duration / 2) / 29;
+	velocidad = (cm / duration);
+        
+        Serial.print('\n');
+        Serial.println("Distancia");
+	Serial.print(cm);
+	Serial.println(" cm");
+  	Serial.print('\n'); 
+  	Serial.println("Velocidad ");
+        Serial.print(duration / 1000);
+  	Serial.print('\n');
+	delay(1000);
+}
